658444b976d6c9254dc5fa0bce6c7340fa5f0a42
Sesen8 <yonass@spu.edu>
2023-11-22 19:03:39 -0800

    added more countries
    thinking about added a user

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,11 @@ use std::io;
 
 #[derive(Debug, Clone, Copy)]
 enum Country {
-    USA,
-    CANADA,
-    UK,
-    ITALY,
-    GERMANY,
-    MEXICO,
-    HONDURAS,
-    COLOMBIA,
-    ERITREA,
-    ETHIOPIA,
-    SPAIN,
-    FRANCE,
-    INDIA,
-    AUSTRALIA,
-    BRAZIL,
-    JAPAN,
-    SOUTH_AFRICA,
-    RUSSIA,
-    ARGENTINA,
-    TURKEY,
+    USA, CANADA, UK, ITALY, GERMANY, MEXICO, HONDURAS, COLOMBIA, ERITREA, ETHIOPIA, SPAIN,
+    FRANCE, INDIA, AUSTRALIA, BRAZIL, JAPAN, SOUTH_AFRICA, RUSSIA, ARGENTINA, TURKEY,
+    NIGERIA, EGYPT, PAKISTAN, SAUDI_ARABIA, SOUTH_KOREA, INDONESIA, VIETNAM,
+    MOROCCO, UKRAINE, POLAND, SWEDEN, NORWAY, NEW_ZEALAND,
+    THAILAND, MALAYSIA, PERU, CHILE, SWITZERLAND,
     
 }
 
@@ -57,6 +42,24 @@ impl Country {
             Country::RUSSIA => "Russia",
             Country::ARGENTINA => "Argentina",
             Country::TURKEY => "Turkey",
+            Country::NIGERIA => "Nigeria",
+            Country::EGYPT => "Egypt",
+            Country::PAKISTAN => "Pakistan",
+            Country::SAUDI_ARABIA => "Saudi Arabia",
+            Country::SOUTH_KOREA => "South Korea",
+            Country::INDONESIA => "Indonesia",
+            Country::VIETNAM => "Vietnam",
+            Country::MOROCCO => "Morocco",
+            Country::UKRAINE => "Ukraine",
+            Country::POLAND => "Poland",
+            Country::SWEDEN => "Sweden",
+            Country::NORWAY => "Norway",
+            Country::NEW_ZEALAND => "New Zealand",
+            Country::THAILAND => "Thailand",
+            Country::MALAYSIA => "Malaysia",
+            Country::PERU => "Peru",
+            Country::CHILE => "Chile",
+            Country::SWITZERLAND => "Switzerland",
             
         }
     }
@@ -84,6 +87,24 @@ impl Country {
             Country::RUSSIA => "Moscow",
             Country::ARGENTINA => "Buenos Aires",
             Country::TURKEY => "Ankara",
+            Country::NIGERIA => "Abuja",
+            Country::EGYPT => "Cairo",
+            Country::PAKISTAN => "Islamabad",
+            Country::SAUDI_ARABIA => "Riyadh",
+            Country::SOUTH_KOREA => "Seoul",
+            Country::INDONESIA => "Jakarta",
+            Country::VIETNAM => "Hanoi",
+            Country::MOROCCO => "Rabat",
+            Country::UKRAINE => "Kyiv",
+            Country::POLAND => "Warsaw",
+            Country::SWEDEN => "Stockholm",
+            Country::NORWAY => "Oslo",
+            Country::NEW_ZEALAND => "Wellington",
+            Country::THAILAND => "Bangkok",
+            Country::MALAYSIA => "Kuala Lumpur",
+            Country::PERU => "Lima",
+            Country::CHILE => "Santiago",
+            Country::SWITZERLAND => "Bern",
             
         }
     }
@@ -98,12 +119,18 @@ impl Country {
             Country::ERITREA | Country::ETHIOPIA => "Africa",
             Country::SPAIN => "Europe",
             Country::INDIA => "Asia",
-            Country::AUSTRALIA => "Australia",
-            Country::BRAZIL | Country::ARGENTINA => "South America",
+            Country::AUSTRALIA | Country::NEW_ZEALAND => "Australia",
+            Country::BRAZIL | Country::ARGENTINA | Country::PERU | Country::CHILE => "South America",
             Country::JAPAN => "Asia",
-            Country::SOUTH_AFRICA => "Africa",
-            Country::RUSSIA => "Europe/Asia",
-            Country::TURKEY => "Asia/Europe",
+            Country::SOUTH_AFRICA | Country::MOROCCO => "Africa",
+            Country::RUSSIA | Country::UKRAINE => "Europe",
+            Country::POLAND | Country::SWEDEN | Country::NORWAY => "Europe",
+            Country::THAILAND | Country::MALAYSIA => "Asia",
+            Country::NIGERIA | Country::EGYPT | Country::SAUDI_ARABIA => "Africa",
+            Country::SOUTH_KOREA | Country::INDONESIA | Country::VIETNAM => "Asia",
+            Country::SWITZERLAND => "Europe",
+            Country::TURKEY => "Asia",
+            Country::PAKISTAN => "Asia",
             
         }
     }
@@ -112,10 +139,10 @@ impl Country {
     fn language(&self) -> &'static str {
         match *self {
             Country::USA => "English",
-            Country::CANADA => "English and French",
-            Country::UK | Country::AUSTRALIA => "English",
+            Country::CANADA => "English",
+            Country::UK | Country::AUSTRALIA | Country::NEW_ZEALAND => "English",
             Country::ITALY => "Italian",
-            Country::GERMANY => "German",
+            Country::GERMANY | Country::SWITZERLAND => "German",
             Country::MEXICO => "Spanish",
             Country::HONDURAS | Country::COLOMBIA => "Spanish",
             Country::ERITREA => "Tigrinya",
@@ -124,11 +151,23 @@ impl Country {
             Country::FRANCE => "French",
             Country::INDIA => "Hindi",
             Country::BRAZIL => "Portuguese",
-            Country::JAPAN => "Japanese",
-            Country::SOUTH_AFRICA => "Multilingual",
-            Country::RUSSIA => "Russian",
             Country::ARGENTINA => "Spanish",
             Country::TURKEY => "Turkish",
+            Country::NIGERIA => "English",
+            Country::EGYPT => "Arabic",
+            Country::PAKISTAN => "Urdu",
+            Country::SAUDI_ARABIA => "Arabic",
+            Country::SOUTH_KOREA => "Korean",
+            Country::INDONESIA => "Indonesian",
+            Country::VIETNAM => "Vietnamese",
+            Country::JAPAN => "Japanese",
+            Country::MOROCCO => "Arabic",
+            Country::UKRAINE | Country::POLAND | Country::SWEDEN | Country::NORWAY => "English",
+            Country::THAILAND => "Thai",
+            Country::MALAYSIA => "Malay",
+            Country::PERU | Country::CHILE => "Spanish",
+            Country::RUSSIA => "Russian",
+            Country::SOUTH_AFRICA => "English",
             
         }
     }
@@ -136,26 +175,21 @@ impl Country {
 
 fn create_countries() -> Vec<Country> {
     vec![
-        Country::USA,
-        Country::CANADA,
-        Country::UK,
-        Country::ITALY,
-        Country::GERMANY,
-        Country::MEXICO,
-        Country::HONDURAS,
-        Country::COLOMBIA,
-        Country::ERITREA,
-        Country::ETHIOPIA,
-        Country::SPAIN,
-        Country::FRANCE,
-        Country::INDIA,
-        Country::AUSTRALIA,
-        Country::BRAZIL,
-        Country::JAPAN,
-        Country::SOUTH_AFRICA,
-        Country::RUSSIA,
-        Country::ARGENTINA,
-        Country::TURKEY,
+        Country::USA, Country::CANADA, Country::UK,
+        Country::ITALY, Country::GERMANY, Country::MEXICO,
+        Country::HONDURAS, Country::COLOMBIA, Country::ERITREA,
+        Country::ETHIOPIA, Country::SPAIN, Country::FRANCE,
+        Country::INDIA, Country::AUSTRALIA,
+        Country::BRAZIL, Country::JAPAN, Country::SOUTH_AFRICA,
+        Country::RUSSIA, Country::ARGENTINA,
+        Country::TURKEY, Country::NIGERIA, Country::EGYPT,
+        Country::PAKISTAN, Country::SAUDI_ARABIA,
+        Country::SOUTH_KOREA, Country::INDONESIA,
+        Country::VIETNAM, Country::MOROCCO,
+        Country::UKRAINE, Country::POLAND, Country::SWEDEN,
+        Country::NORWAY, Country::NEW_ZEALAND, Country::THAILAND,
+        Country::MALAYSIA, Country::PERU,
+        Country::CHILE, Country::SWITZERLAND,
     ]
     
 }
@@ -172,26 +206,20 @@ fn ask_question(country: &Country, difficulty: &Difficulty) {
 // USING RAND
 fn random_country() -> Country {
     let countries = vec![
-        Country::USA,
-        Country::CANADA,
-        Country::UK,
-        Country::ITALY,
-        Country::GERMANY,
-        Country::MEXICO,
-        Country::HONDURAS,
-        Country::COLOMBIA,
-        Country::ERITREA,
-        Country::ETHIOPIA,
-        Country::SPAIN,
-        Country::FRANCE,
-        Country::INDIA,
-        Country::AUSTRALIA,
-        Country::BRAZIL,
-        Country::JAPAN,
-        Country::SOUTH_AFRICA,
-        Country::RUSSIA,
-        Country::ARGENTINA,
-        Country::TURKEY,
+        Country::USA, Country::CANADA, Country::UK, Country::ITALY,
+        Country::GERMANY, Country::MEXICO, Country::HONDURAS,
+        Country::COLOMBIA, Country::ERITREA, Country::ETHIOPIA,
+        Country::SPAIN, Country::FRANCE, Country::INDIA,
+        Country::AUSTRALIA, Country::BRAZIL,
+        Country::JAPAN, Country::SOUTH_AFRICA, Country::RUSSIA,
+        Country::ARGENTINA, Country::TURKEY, Country::NIGERIA,
+        Country::EGYPT, Country::PAKISTAN,
+        Country::SAUDI_ARABIA, Country::SOUTH_KOREA, Country::INDONESIA,
+        Country::VIETNAM, Country::MOROCCO,
+        Country::UKRAINE, Country::POLAND, Country::SWEDEN,
+        Country::NORWAY, Country::NEW_ZEALAND, Country::THAILAND,
+        Country::MALAYSIA, Country::PERU,
+        Country::CHILE, Country::SWITZERLAND,
     ];
     let mut rng = rand::thread_rng();
     let index = rng.gen_range(0..countries.len());
@@ -218,13 +246,13 @@ fn main() {
     loop {
         
         println!(" ");
-        println!("Choose a difficulty level (Easy, Medium, Hard), or type 'exit' to quit:");
+        println!("Choose a difficulty level!!! (Easy, Medium, Hard), or type 'exit' to quit:");
         let mut user_input = String::new();
-        io::stdin().read_line(&mut user_input).expect("Failed to read line");
+        io::stdin().read_line(&mut user_input).expect("huh? try again please?");
         let difficulty_choice = user_input.trim().to_lowercase();
 
         if difficulty_choice == "exit" {
-            println!("Thanks for playing. Goodbye!");
+            println!("Thanks for playing!!!! Have a great day!!!!!");
             break;
         }
 
@@ -233,7 +261,7 @@ fn main() {
             "medium" => Difficulty::Medium,
             "hard" => Difficulty::Hard,
             _ => {
-                println!("Invalid difficulty level. Please choose Easy, Medium, or Hard.");
+                println!("Thats not on my list hmm. Please choose Easy, Medium, or Hard.");
                 continue;
             }
         };
@@ -245,7 +273,7 @@ fn main() {
             ask_question(&selected_country, &player_difficulty);
 
             let mut user_input = String::new();
-            io::stdin().read_line(&mut user_input).expect("Failed to read line");
+            io::stdin().read_line(&mut user_input).expect("huh? try again please?");
             let user_guess = user_input.trim().to_lowercase();
 
             
@@ -255,15 +283,15 @@ fn main() {
                         break; 
                     }
                     if user_guess == "exit" {
-                        println!("Thanks for playing. Goodbye!");
+                        println!("Thanks for playing!!!! Have a great day!!!!");
                         return; 
                     }
 
                     if user_guess == selected_country.continent().to_lowercase() {
-                        println!("Correct!");
+                        println!("You got it Correct!!!!!!");
                         score += 1;
                     } else {
-                        println!("Incorrect. The correct answer is {}.", selected_country.continent());
+                        println!("Aww thats Incorrect :( The correct answer is {}.", selected_country.continent());
                     }
                 }
 
@@ -273,15 +301,15 @@ fn main() {
                         break; 
                     }
                     if user_guess == "exit" {
-                        println!("Thanks for playing. Goodbye!");
+                        println!("Thanks for playing!!!! Have a great day!!!!");
                         return; 
                     }
 
                     if user_guess == selected_country.capital().to_lowercase() {
-                        println!("Correct!");
+                        println!("You got it Correct!!!!!!");
                         score += 2;
                     } else {
-                        println!("Incorrect. The correct answer is {}.", selected_country.capital());
+                        println!("Aww thats Incorrect :( The correct answer is {}.", selected_country.capital());
                     }
                 }
 
@@ -292,15 +320,15 @@ fn main() {
                         break; 
                     }
                     if user_guess == "exit" {
-                        println!("Thanks for playing. Goodbye!");
+                        println!("Thanks for playing!!!! Have a great day!!!!");
                         return; 
                     }
 
                     if user_guess == selected_country.language().to_lowercase() {
-                        println!("Correct!");
+                        println!("You got it Correct!!!!!");
                         score += 3;
                     } else {
-                        println!("Incorrect. The correct answer is {}.", selected_country.language());
+                        println!("Aww thats Incorrect :( The correct answer is {}.", selected_country.language());
                     }
                 }
             }
